a9fbe751890f2a8309f520992a4ba8999a7b19f7
DiwanshGupta <guptadiwansh@gmail.com>
2025-01-22 01:27:12 +0530

    fixing bugs

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(morgan('dev'))
 app.use(express.json());
 
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: process.env.FRONTEND_URL, 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
   allowedHeaders: ['Content-Type', 'Authorization'],  
   credentials: true, 
